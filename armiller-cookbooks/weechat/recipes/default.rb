7f10ff582a24bf2c4ce2985581509ea8ec7713ea
Anthony <anthonymiller@outlook.com>
2013-09-14 15:38:58 -0700

    adding weechat cookbook

---
+++ armiller-cookbooks/weechat/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: weechat
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "weechat" do
+    action :install
+end
+
+
